3d5a614b45518d086b0a7a80cd2e4f4929c6c9fc
pablohernanaraujo <pablohernanaraujo@gail.com>
2016-01-30 11:03:15 -0300

    sexto commit

--- app/scripts/controllers/inicio.js
+++ app/scripts/controllers/inicio.js
@@ -1,8 +1,22 @@
 'use strict';
 
 angular.module('arteandoApp')
-  .controller('InicioCtrl', function ($scope, $rootScope) {
+  .controller('InicioCtrl', function ($scope, $rootScope, $timeout) {
     
     $rootScope.PAGE = 'inicio';
+
+    $timeout(function() {
+	    var ancho = $( document ).width();
+
+	    if(ancho>1280){
+	        $('.slider').slider({'full_width': true, height:730});
+	    }
+	    if(ancho<=1280 && ancho>800){
+	        $('.slider').slider({'full_width': true, height:640});
+	    }
+	    if(ancho<=800){
+	        $('.slider').slider({'full_width': true, height:500});
+	    }
+	},0);
 
   });
